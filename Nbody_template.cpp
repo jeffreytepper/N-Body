2d71e3e8c69245c103c1b0564a6bd069f1545807
jeffreytepper <teppermax@gmail.com>
2020-09-16 23:08:00 -0500

    add changes to hw

--- Nbody_template.cpp
+++ Nbody_template.cpp
@@ -1,3 +1,21 @@
+/*
+Template for the Nbody simuilation excercise
+Copyright (C) 2019  Patrick Diehl
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include<iostream>
 #include <fstream>
 #include <string>
@@ -6,7 +24,7 @@
 #include <cmath>
 
 #define TYPE double
-
+#define GENTYPE double
 /*
  *  Task 1:
  *  Write a generic struct for a vector
@@ -14,12 +32,14 @@
  *  Add a constructor which initializes the vector to zero
  *  Overload the operators + and - for a second vector and the operator * for multplication with a scalar
  */
+template <typename T>
 struct vector
 {
     
-    double x,y,z;
+    T x,y,z;
     
     vector(): x(0.0), y(0.0), z(0.0){};
+    vector(T x, T y, T z): x(x), y(y), z(z){};
     
     bool operator==(const vector& rhs) const {
     return x == rhs.x
@@ -51,22 +71,27 @@ struct vector
         return v;
     }
     
-    double norm(){
+    std::string to_string(){
+        
+        return std::to_string(x) + ", " + std::to_string(y) + ", " + std::to_string(z);
+    }
+    
+    T norm(){
         return std::sqrt(x*x + y*y + z*z);
     }
 
-    friend std::istream& operator>>(std::istream&, vector&);
-    friend std::ostream& operator<<(std::ostream&, vector&);
+    friend std::istream& operator>>(std::istream&, vector<GENTYPE>&);
+    friend std::ostream& operator<<(std::ostream&, vector<GENTYPE>&);
 };
 
 extern "C++"
-std::istream& operator>>(std::istream& in, vector& v) {
+std::istream& operator>>(std::istream& in, vector<GENTYPE>& v) {
     return in >> v.x >> v.y >> v.z;
 }
 
 
 extern "C++"
-std::ostream& operator<<(std::ostream& out, vector& v) {
+std::ostream& operator<<(std::ostream& out, vector<GENTYPE>& v) {
     auto precision = out.precision();
     auto width = out.width();
     out << std::setw(width) << std::setprecision(precision) << v.x << "  ";
@@ -83,109 +108,113 @@ std::ostream& operator<<(std::ostream& out, vector& v) {
  */
 
 
-class Nbody
-{
+class Nbody{
 
-private:
+    private:
 
-double gc;
-size_t n;
-TYPE timeStepSize;
-std::vector<double> masses;
-std::vector<vector> positions;
-std::vector<vector> velocities;
-std::vector<vector> velocitiesOld;
-std::vector<vector> forces;
+    double gc;
+    size_t n;
+    TYPE timeStepSize;
+    std::vector<double> masses;
+    std::vector<vector<GENTYPE>> positions;
+    std::vector<vector<GENTYPE>> velocities;
+    std::vector<vector<GENTYPE>> velocitiesOld;
+    std::vector<vector<GENTYPE>> forces;
 
-public:
+    public:
 
-size_t timeSteps;
+    size_t timeSteps;
 
-//Method to compute the acceleration
-void computeForces(){
-    vector sum = vector();
-    for(size_t i = 0; i < n; i++){
-        for(size_t j = 0; j < n; i++){
-            if(i != j){
-                sum = sum + (positions[j] - positions[i]) * ((1 / std::pow((positions[j] - positions[i]).norm(),3)) * (gc * masses[j]));
+    //Method to compute the acceleration
+    void computeForces(){
+        vector<GENTYPE> sum = vector<GENTYPE>();
+        for(size_t i = 0; i < n; i++){
+            for(size_t j = 0; j < n; j++){
+                if(i != j){
+                    sum = sum + (positions[j] - positions[i]) * ((1 / std::pow((positions[j] - positions[i]).norm(),3)) * (gc * masses[j]));
+                }
             }
+            forces[i] = sum;
+            sum = vector<GENTYPE>();
         }
-        forces[i] = sum;
-        sum = vector();
     }
-}
 
-//Method to compute the velocity
-void computeVelocities(){
-    for(size_t i = 0; i < n; i++){
-        velocities[i] = velocitiesOld[i] + forces[i] * (1/masses[i]) * timeStepSize;
+    //Method to compute the velocity
+    void computeVelocities(){
+        for(size_t i = 0; i < n; i++){
+            velocities[i] = velocitiesOld[i] + forces[i] * (1/masses[i]) * timeStepSize;
+        }
+         std::copy(velocities.begin(),velocities.end(),velocitiesOld.begin());
     }
-     std::copy(velocities.begin(),velocities.end(),velocitiesOld.begin());
-}
 
-//Method to update the positions
-void updatePositions(){
-    for(size_t i = 0; i < n; i++){
-        positions[i] = positions[i] + velocities[i] * timeStepSize;
+    //Method to update the positions
+    void updatePositions(){
+        for(size_t i = 0; i < n; i++){
+            positions[i] = positions[i] + velocities[i] * timeStepSize;
+        }
     }
-}
 
-//Method to detect collisions between bodys
-void detectCollisions(){
+    //Method to detect collisions between bodys
+    void detectCollisions(){
 
-  for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
+      for (int i = 0; i < n; ++i) {
+          for (int j = i + 1; j < n; ++j) {
                 if (positions[i] == positions[j]) {
                     std::swap(velocities[i], velocities[j]);
                 }
             }
         }
+    }
 
-}
+    public:
 
-public:
+    //Constructor
+    Nbody(std::string& fileName) {
 
-//Constructor
-Nbody(std::string& fileName) {
-
-std::ifstream ifs(fileName);
+        std::ifstream ifs(fileName);
         if (!ifs.is_open()) {
             throw std::runtime_error("Could not open " + fileName + "!");
         }
+            
+
+    //Read the gravity, the number of bodies, and the number of timesteps
+        ifs >> gc >> n >> timeSteps >> timeStepSize;
+
+        masses.resize(n);
         
+        positions.resize(n);
+        std::fill(positions.begin(), positions.end(), vector<GENTYPE>());
+        
+        velocities.resize(n);
+        std::fill(velocities.begin(), velocities.end(), vector<GENTYPE>());
+        
+        velocities.resize(n);
+        std::fill(velocities.begin(), velocities.end(), vector<GENTYPE>());
+        
+        velocitiesOld.resize(n);
+        std::fill(velocitiesOld.begin(), velocitiesOld.end(), vector<GENTYPE>());
+        
+        forces.resize(n);
+        std::fill(forces.begin(), forces.end(), vector<GENTYPE>());
 
-//Read the gravity, the number of bodies, and the number of timesteps
-ifs >> gc >> n >> timeSteps >> timeStepSize;
-
-masses.resize(n);
-positions.resize(n);
-std::fill(positions.begin(), positions.end(), vector());
-velocities.resize(n);
-std::fill(velocities.begin(), velocities.end(), vector());
-velocities.resize(n);
-std::fill(velocities.begin(), velocities.end(), vector());
-velocitiesOld.resize(n);
-std::fill(velocitiesOld.begin(), velocitiesOld.end(), vector());
-forces.resize(n);
-std::fill(forces.begin(), forces.end(), vector());
-
-for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i) {
             ifs >> masses[i] >> positions[i] >> velocities[i];
         }
- 
+         
         std::cout << "Contents of " << fileName << std::endl;
         std::cout << gc << ' ' << n << ' ' << timeSteps << std::endl;
         for (int i = 0; i < n; ++i) {
             std::cout << masses[i] << std::endl;
             std::cout << positions[i] << std::endl;
             std::cout << velocities[i] << std::endl;
+            
         }
         std::cout << std::endl << "Data   :      x          y          z    |     vx         vy         vz" << std::endl;
 
-}
+    }
 
 
-friend std::ostream& operator<<(std::ostream&, Nbody&);
+    friend std::ostream& operator<<(std::ostream&, Nbody&);
 
 };
 
@@ -203,11 +232,13 @@ std::ostream& operator<<(std::ostream& out, Nbody& nb) {
 
 int main(){
 
-std::string fileName = "nbody.txt";
+    std::string fileName = "nbody.txt";
     
     Nbody nb(fileName);
-
+    
     for(size_t t = 0; t < nb.timeSteps; t++){
+        
+        
 
         std::cout << std::endl << "Cycle " << t << std::endl;
         nb.computeForces();
